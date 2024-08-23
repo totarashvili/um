4f65d03d63fd84115ee9a7abb26f38b74640d604
totarashvili <toarashvili@clp.ge>
2024-08-23 13:24:07 +0400

    asas

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        myfont: ['NEOMETRA', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
