9f6b82027ae317c1ec929aa1e3966e1730366fb2
totarashvili <toarashvili@clp.ge>
2024-08-27 01:21:59 +0400

    markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,26 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      // fontFamily: {
+      //   'helvetica': ['Helvetica Neue LT GEO', 'sans-serif'],
+      //   'helvetica-bold': ['Helvetica Neue Bold', 'sans-serif'],
+      //   'umgbold': ['UMgBold', 'sans-serif'],
+      //   'bpg-caps': ['BPG-WEB-001-Caps', 'sans-serif'],
+      // },
+      /* Font Face for Helvetica Neue LT GEO 25 UltLt */
       fontFamily: {
-        myfont: ['NEOMETRA', 'sans-serif'],
+        'helvetica-neue': ['Helvetica Neue LT GEO', 'sans-serif'],
+        'Umgeo': ['Umgeo','sans-serif']
+      },
+      fontWeight: {
+        'ultralight': 200,
+        'thin': 300,
+        'light':400,
+        'normal': 500,
+        'medium': 600,
+        'bold': 700,
+        'heavy': 800,
+        'black': 900,
       },
     },
   },
